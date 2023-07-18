4ace83e76ac1f852138edae55faa93eea299eb9c
StarcevAlexander <116572797+StarcevAlexander@users.noreply.github.com>
2023-07-18 19:42:20 +0800

    нормальный вариант звёздочек через чекбоксы

--- заготовки/промисы/промисы-тренировка/script/script.js
+++ заготовки/промисы/промисы-тренировка/script/script.js
@@ -7,13 +7,41 @@ console.log('hello 2');
 
 //движок V8 С++
 
-let fetchRes = fetch('https://fakestoreapi.com/products') //проходит время
+/* let fetchRes = fetch('https://fakestoreapi.com/products') //проходит время
     .then(res => res.json()) //сервер вернёт обьект респонc
     .then(productsList => {
         productsList.map((product) => productsListDOM.innerHTML +=  `<div class="products-item"> <div class="title">${product.title}</div> <img src='${product.image}'></div>`)
     })
     .catch(err => console.log('err', err))
+ */
 
+fetch('https://fakestoreapi.com/users', {
+    method: "POST",
+    body: JSON.stringify(
+        {
+            email: 'John@gmail.com',
+            username: 'johnd',
+            password: 'm38rmF$',
+            name: {
+                firstname: 'John',
+                lastname: 'Doe'
+            },
+            address: {
+                city: 'kilcoole',
+                street: '7835 new road',
+                number: 3,
+                zipcode: '12926-3874',
+                geolocation: {
+                    lat: '-37.3159',
+                    long: '81.1496'
+                }
+            },
+            phone: '1-570-236-7033'
+        }
+    )
+})
+    .then(res => res.json())
+    .then(json => console.log(json))
 
 //.finally(()=> isLoading = true, console.log(123)) //файнали сработает всегда, независимо от резолв/реджект
 
